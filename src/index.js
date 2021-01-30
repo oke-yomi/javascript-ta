f06165c3869aed7c424cfe82cd91c4af1114b8f0
Oluwayomi Oke <okeoluwayomi9@gmail.com>
2021-01-30 15:41:06 +0100

    Solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 // complete the function
 function palindrom(str) {
-  // code goes here
+  
+  
+  var rExp = /[\W_]/g;   // Using the RegExp to remove unwanted characters from it
+  var lowerStr = str.toLowerCase().replace(rExp, ''); // Changing the string to lowercase, and replacing
+  var reverseStr = lowerStr.split('').reverse().join(''); // .split() is used to split each string, .reverse() is used to reverse the split words, .join() adds the reversed words together
+  return reverseStr === lowerStr;
 }
+
+// code goes here
+
+
+
+
+
 
 function solution(arg) {
   return palindrom(arg);
